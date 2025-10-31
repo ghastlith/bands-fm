72b326d7798fd027b665875813629c9cd3f968b8
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-10-31 20:24:46 -0300

    fix: swap string to use correct type

--- src/main/java/dentsu/bands/external/model/Album.java
+++ src/main/java/dentsu/bands/external/model/Album.java
@@ -1,12 +1,13 @@
 package dentsu.bands.external.model;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public record Album (
     UUID id,
     String name,
-    String releasedDate,
+    OffsetDateTime releasedDate,
     String image,
     Band band,
     List<Track> tracks
