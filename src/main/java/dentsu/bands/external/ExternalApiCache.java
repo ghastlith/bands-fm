b062adba8f5697501e2f46efd1c37157a81eb610
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-10-31 20:20:38 -0300

    feat: add albums cache

--- src/main/java/dentsu/bands/external/ExternalApiCache.java
+++ src/main/java/dentsu/bands/external/ExternalApiCache.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
+import dentsu.bands.external.model.Album;
 import dentsu.bands.external.model.Band;
 import lombok.RequiredArgsConstructor;
 
@@ -19,13 +20,22 @@ public class ExternalApiCache {
 
     private static final String SINGLETON_KEY = "only";
 
-    private final Cache<String, List<Band>> cache = Caffeine.newBuilder()
+    private final Cache<String, List<Band>> bandCache = Caffeine.newBuilder()
+        .expireAfterWrite(Duration.ofMinutes(10))
+        .maximumSize(1)
+        .build();
+
+    private final Cache<String, List<Album>> albumCache = Caffeine.newBuilder()
         .expireAfterWrite(Duration.ofMinutes(10))
         .maximumSize(1)
         .build();
 
     public List<Band> getAllBands() {
-        return cache.get(SINGLETON_KEY, key -> externalApiService.getAllBands());
+        return bandCache.get(SINGLETON_KEY, key -> externalApiService.getAllBands());
+    }
+
+    public List<Album> getAllAlbums() {
+        return albumCache.get(SINGLETON_KEY, key -> externalApiService.getAllAlbums());
     }
 
 }
